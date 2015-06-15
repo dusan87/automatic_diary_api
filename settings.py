0d9fd7a10db01ec827ebd5c3ecfd6dcd1a9f0984
Dusan Ristic <dusan.ristic1987@gmail.com>
2015-06-15 13:07:48 +0200

    Clean up the project config and requirements, updating the same with new django version and running it after a long time

--- settings.py
+++ settings.py
@@ -11,13 +11,13 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'ad_mysql',                # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'diary-db',                # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': 'dusan87',
+        'USER': '',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '3306',                      # Set to empty string for default.
+        'PORT': '',                      # Set to empty string for default.
     }
 }
 
