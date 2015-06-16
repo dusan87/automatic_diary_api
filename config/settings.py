b07933774f8aa10267d68a6088f9a7ab556e3c86
Dusan Ristic <dusan.ristic1987@gmail.com>
2015-06-16 18:33:47 +0200

    Re-structuring part 2

--- config/settings.py
+++ config/settings.py
@@ -117,10 +117,10 @@ MIDDLEWARE_CLASSES = (
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'diary_api.urls'
+ROOT_URLCONF = 'automatic_diary.urls'
 
 # Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'diary_api.wsgi.application'
+WSGI_APPLICATION = 'automatic_diary.wsgi.application'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
