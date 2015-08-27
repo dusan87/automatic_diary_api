74866f9adc644c7a25522085e847a4d13727039b
Dusan Ristic <dusan.ristic1987@gmail.com>
2015-08-27 15:31:46 +0200

    Adding seriazliers for new models

--- api/serializers.py
+++ api/serializers.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import AndroidUser
+from .models import AndroidUser, Location, UserLocations
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -11,3 +11,20 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('id', 'username', 'first_name',
                   'last_name', 'country', 'city',
                   'image', 'gender', 'birth_day',)
+
+
+class LocationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model  = Location
+        fields = ('id','lng', 'lat')
+
+
+class UserLocationsSerializer(serializers.ModelSerializer):
+
+    location = LocationSerializer()
+    user = UserSerializer()
+
+    class Meta:
+        model  = UserLocations
+        fields = ('created_at', 'location', 'user')
