528a1eba4e199526444985cc61f2cb9a59d623fb
Dusan Ristic <dusan.ristic1987@gmail.com>
2015-09-01 12:34:59 +0200

    Adding new endpoints

--- api/urls.py
+++ api/urls.py
@@ -5,12 +5,16 @@ from . import api
 
 urlpatterns = [
     url(r'^create_user/$', api.CreateUser.as_view()),
-    url(r'^login_user/$', 'api.views.login_user'),
     url(r'^auth_user/$', api.AuthView.as_view()),
     url(r'^validate/$', api.UserValidationView.as_view()),
     url(r'^users/$', api.ListUsersView.as_view()),
     url(r'^location/$', api.LocationView.as_view()),
     url(r'^follow/(?P<pk>[0-9]+)/$', api.FollowView.as_view()),
+    url(r'^interact/$', api.InteractionView.as_view()),
+    url(r'^follow/(?P<pk>[0-9]+)/$', api.FollowView.as_view()),
+
+    # Old endpoints
+    url(r'^login_user/$', 'api.views.login_user'),
     url(r'^check_user/$', 'api.views.check_user'),
     url(r'^all_users/$', 'api.views.all_users'),
     url(r'^add_follower/$', 'api.views.add_users_follow'),
